678db84ccfe530fd4eb3d735651e89116c29db57
Reds <sahid.rojas@gmail.com>
2019-04-01 21:58:59 -0600

    Se agrega la logica de hilos a la clase sala

--- sala.cpp
+++ sala.cpp
@@ -3,6 +3,12 @@
 #include <iostream>
 
 using namespace std;
+void sala::run()
+{
+    //aqui iria la logica de la sala
+    count<<"el hilo funciona"<<endl;
+}
+
 sala::sala()
 {
     
@@ -14,16 +20,18 @@ void sala::agregar_jugador(char ip[])
         Jugadores[4-turno]=ip;
         cout<<Jugadores[4-turno]<<endl;
         turno=turno-1;
-
     }
     else {
         cout<<"ya no queda campo para los demas jugadores"<<endl;
     }
-    
-
 }
 
 char sala::Dame_ip(int valor)
 {
     return *Jugadores[valor];
+}
+
+void sala::empezar()
+{
+    this->start();
 }
--- sala.h
+++ sala.h
@@ -1,17 +1,20 @@
 #ifndef SALA_H
 #define SALA_H
+#include <QThread>
 
-
-class sala
+class sala:public QThread
 {
 private:
 
     char *Jugadores[4];
     int turno=4;
+    QThread hola;
+   void run();
 public:
     sala();
      void agregar_jugador(char ip[]);
      char Dame_ip(int valor );
+     void empezar();
 };
 
 #endif // SALA_H
